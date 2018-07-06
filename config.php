e8bdf2eeec80aee1a32d3a68b9d8af6779953daf
yogin21 <38465727+yogin21@users.noreply.github.com>
2018-07-06 17:31:06 +0530

    update config.php

--- config.php
+++ config.php
@@ -8,7 +8,7 @@
 function config($key = '')
 {
     $config = [
-        'name' => 'Simple new PHP Website',
+        'name' => 'Simple new123 PHP Website',
         'nav_menu' => [
             '' => 'Home',
             'about-us' => 'About Us',
